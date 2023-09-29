cb99a0c39964661694e034b07906be50daef455b
vanessabmeira <vanessabmeira@outlook.com>
2023-09-29 15:11:25 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1Aluno/Aluno.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1Aluno
+{
+    internal class Aluno
+    {
+        public int matricula;
+        public string nome;
+        private double prova1;
+        private double prova2;
+        private double trabalho1;
+
+
+        /// <summary>
+        /// Método contrutor utilizado para guardar as informações (nome e matricula) do aluno. 
+        /// </summary>
+        /// <param name="matricula"></param>
+        /// <param name="nome"></param>
+        internal Aluno(int matricula, string nome)
+        {
+            this.matricula = matricula;
+            this.nome = nome;
+
+        }
+
+        /// <summary>
+        /// Método criado para setar "valores" necessarios para o exercicio..  
+        /// </summary>
+        /// <param name="notaProva1"></param>
+        /// <param name="notaProva2"></param>
+        /// <param name="notaTrabalho1"></param>
+        internal void definirNotas(double notaProva1, double notaProva2, double notaTrabalho1)
+        {
+            prova1 = notaProva1;
+            prova2 = notaProva2;
+            trabalho1 = notaTrabalho1;
+        }
+        /// <summary>
+        /// Este método tem como funcionalidade de calcular a média ponderada.
+        /// </summary>
+        /// <returns></returns>
+        internal double definirMedia()
+        {
+            double media = (prova1 * 2.5 + prova2 * 2.5 + trabalho1 * 2) / 7;
+            return media;
+
+        }
+
+        /// <summary>
+        /// Método que defini se o aluno foi aprovado ou reprovado. 
+        /// </summary>
+        /// <param name="media"></param>
+        /// <returns></returns>
+        public bool definirAprovacao(double media)
+        {
+            if (media >= 6)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Programacao Orientada a Objetos l/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+/* Escreva uma classe cujos objetos representam alunos matriculados em uma disciplina.
+         * Cada objeto dessa classe deve guardar os seguintes dados do aluno:
+         * matrícula, nome, 2 notas de prova e 1 nota de trabalho.
+         * Construa os seguintes métodos para esta classe:
+         * métodos construtores,
+         * registrar nota de cada prova e trabalho,
+         *  calcula a média final do aluno (cada prova tem peso 2,5 e o trabalho tem peso 2).
+         * Teste na main, se atente para o que precisar ser public, private, parametros, retornos.. 
+         */
+
+namespace Exercicio1Aluno
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno(2154, "João");
+
+            aluno1.definirNotas(5.3, 6.5, 7);
+
+            double media = Math.Round(aluno1.definirMedia(), 2);
+
+            Console.WriteLine($"O Aluno {aluno1.nome}, matricula {aluno1.matricula} ficou com a média {media}.");
+
+            if (aluno1.definirAprovacao(media))
+            {
+                Console.WriteLine("O aluno foi Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("O aluno foi Reprovado!");
+            }
+
+
+        }
+    }
+}
